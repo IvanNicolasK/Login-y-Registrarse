f733b033fba5d29f5ed8304b0df72a4e1a1005a0
Ivan Karlen <Ivankpomanutd@hotmail.com>
2019-04-27 17:39:23 -0300

    Login y como registrar a un usuario

---
+++ Login/conectarse.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost"; //$host = "localhost:3307";
+$usuario = "root";
+$clave = 11021998; // $clave = '';
+$bd = "loguear";
+
+
+$link = new mysqli($host, $usuario, $clave, $bd);
+
+
+
+
+?>
